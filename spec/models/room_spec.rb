b4cbfddf110dd7a421d8491519b42581d9078363
yusuke-k-03 <sinkuku01@gmail.com>
2020-12-16 11:59:40 +0900

    各モデルのテスト

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  describe '#create' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it "nameの値が存在すれば登録できること" do
+      expect(@room).to be_valid
+    end
+
+    it "nameが空では登録できないこと" do
+      @room.name = nil
+      @room.valid?
+      expect(@room.errors.full_messages).to include("Name translation missing: ja.activerecord.errors.models.room.attributes.name.blank")
+    end
+  end
 end
